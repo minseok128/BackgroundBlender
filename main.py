8800c47b1988c82f6723f82a64906ae40d7a73bc
michang <minseok128128@gmail.com>
2025-01-23 22:13:47 +0900

    Update main.py : server

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from PIL import Image
 import sys
 import os
+from datetime import datetime
+import urllib.request
+import base64
+import json
+import time
+import os
+
+webui_server_url = "http://127.0.0.1:3000"
 
 
-def create_image(image_path, size_x, size_y, pos_x, pos_y, save_path):
+def encode_file_to_base64(path):
+    with open(path, "rb") as file:
+        return base64.b64encode(file.read()).decode("utf-8")
+
+
+def decode_and_save_base64(base64_str, save_path):
+    with open(save_path, "wb") as file:
+        file.write(base64.b64decode(base64_str))
+
+
+def call_api(api_endpoint, **payload):
+    data = json.dumps(payload).encode("utf-8")
+    request = urllib.request.Request(
+        f"{webui_server_url}/{api_endpoint}",
+        headers={"Content-Type": "application/json"},
+        data=data,
+    )
+    response = urllib.request.urlopen(request)
+    return json.loads(response.read().decode("utf-8"))
+
+
+def call_img2img_api(save_path, **payload):
+    response = call_api("sdapi/v1/img2img", **payload)
+    for index, image in enumerate(response.get("images")):
+        save_path = os.path.join(save_path, f"img2img-{datetime.fromtimestamp(time.time()).strftime("%Y%m%d-%H%M%S")}-{index}.png")
+        decode_and_save_base64(image, save_path)
+
+
+def create_prepared_image(image_path, size_x, size_y, pos_x, pos_y):
     temp_dir = "./tmp/"
     os.makedirs(temp_dir, exist_ok=True)
 
@@ -17,9 +53,9 @@ def create_image(image_path, size_x, size_y, pos_x, pos_y, save_path):
         masked_pixels = []
         for pixel in pixels:
             if pixel == (0, 0, 0, 0):
-                masked_pixels.append((255, 255, 255, 255))
+                masked_pixels.append((0, 0, 0, 255))
             else:
-                masked_pixels.append((0, 0, 0, 0))
+                masked_pixels.append((255, 255, 255, 255))
         masked_image = Image.new("RGBA", new_image.size)
         masked_image.putdata(masked_pixels)
         masked_image.save(os.path.join(temp_dir, "img2.png"), "PNG")
@@ -28,6 +64,26 @@ def create_image(image_path, size_x, size_y, pos_x, pos_y, save_path):
         print(f"오류: 파일 {image_path}를 찾을 수 없습니다.")
     except Exception as e:
         print(f"오류 발생: {e}")
+
+
+def generate_image(save_path):
+    init_images = [
+        encode_file_to_base64(r"./tmp/img1.png"),
+    ]
+    batch_size = 2
+    payload = {
+        "prompt": "1girl, blue hair",
+        "seed": 1,
+        "steps": 20,
+        "width": 512,
+        "height": 512,
+        "denoising_strength": 0.5,
+        "n_iter": 1,
+        "init_images": init_images,
+        "batch_size": batch_size if len(init_images) == 1 else len(init_images),
+        "mask": encode_file_to_base64("./tmp/img2.png"),
+    }
+    call_img2img_api(save_path, **payload)
 
 
 if __name__ == "__main__":
@@ -63,8 +119,11 @@ if __name__ == "__main__":
             or image_caption
             + " on a sandy beach, clear blue sky, photorealistic, 8k, highly detailed, trending on artstation"
         )
+
+        create_prepared_image(image_path, size_x, size_y, pos_x, pos_y)
+        generate_image(save_path)
+
     except ValueError:
         print("오류: 올바른 숫자 값을 입력하세요.")
         sys.exit(1)
 
-    create_image(image_path, size_x, size_y, pos_x, pos_y, save_path)
